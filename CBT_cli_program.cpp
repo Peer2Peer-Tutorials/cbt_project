353514b6c5fb42a13c44e43dad041fd51d34e9c6
adaobi15 <93054208+adaobi15@users.noreply.github.com>
2022-11-03 12:33:34 +0100

    cbt question pull

--- CBT_cli_program.cpp
+++ CBT_cli_program.cpp
@@ -1,8 +1,218 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    cout << "Hello, tutees!" << endl;
 
-    return 0;
-}
+	
+
+int main ()
+ { int ans , total_score;
+ char answer;
+
+ 
+ 
+ 	cout << "Quiz can commence";
+ 	
+ 
+	
+
+	cout << " 1.what do you call a portable computer? \n A. Laptop \n B. Bookshop \n C. RAM \n D. CPU \n ";
+    cin >>answer;
+    	
+    
+    if (answer == 'A'  or answer =='a')
+    {
+    	cout << "correct!! \n";
+    	total_score =total_score+1;
+	}
+	
+	else if (answer == 'B' || answer == 'C' || answer == 'D')
+	{
+	 cout << "Incorrect\n";	
+	 total_score =total_score;
+	}
+	else
+	{
+		cout << "Error\n";
+	}
+
+
+	
+		cout << "2.How many legs do insects have? \n A) 1 \n B)3 \n C) 8 \n D) 5 \n ";
+    cin >> answer;
+    
+    if (answer == 'C'  or answer =='c')
+    {
+    	cout << "correct!! \n";
+    	total_score = total_score + 1;
+	}
+	
+	else if (answer == 'A' || answer == 'B' || answer == 'D')
+	{
+	 cout << "Incorrect\n";	
+	 total_score = total_score;
+	}
+	else
+	{
+		cout << "Error\n";
+	}
+
+		cout << "3.How many days are there in a year? \n A) 67 \n B) 365 \n C) 144 \n D) 24 \n ";
+    cin >> answer;
+    
+    if (answer == 'b'  or answer =='B')
+    {
+    	cout << "correct!! \n";
+    	total_score = total_score + 1;
+	}
+	
+	else if (answer == 'A' || answer == 'C' || answer == 'D')
+	{
+	 cout << "Incorrect\n";	
+	 total_score = total_score;
+	}
+	else
+	{
+		cout << "Error\n";
+	}
+	
+	
+		cout << "4.How many players are in a soccer team? \n A) 10 \n B) 11 \n C) 13 \n D) 9 \n ";
+    cin >> answer;
+    
+    if (answer == 'B'  or answer =='b')
+    {
+    	cout << "correct!! \n";
+    	total_score = total_score + 1;
+	}
+	
+	else if (answer == 'A' || answer == 'C' || answer == 'D')
+	{
+	 cout << "Incorrect\n";	
+	 total_score = total_score;
+	}
+	else
+	{
+		cout << "Error\n";
+	}
+	
+	
+		cout << "5.How many letters are in the English alphabet? \n A) 0 \n B) 56 \n C) 14 \n D) 26 \n ";
+    cin >> answer;
+    
+    if (answer == 'D'  or answer =='d')
+    {
+    	cout << "correct!! \n";
+    	total_score = total_score + 1;
+	}
+	
+	else if (answer == 'B' || answer == 'C' || answer == 'A')
+	{
+	 cout << "Incorrect\n";	
+	 total_score = total_score;
+	}
+	else
+	{
+		cout << "Error\n";
+	}
+	
+	
+		cout << "6.What is the name of a shape with 5 sides? \n A) pentagon \n B) decagon \n C) nonagon\n D) polygon \n ";
+    cin >> answer;
+    
+    if (answer == 'A'  or answer =='a')
+    {
+    	cout << "correct!! \n";
+    	total_score = total_score + 1;
+	}
+	
+	else if (answer == 'B' || answer == 'C' || answer == 'D')
+	{
+	 cout << "Incorrect\n";	
+	 total_score = total_score;
+	}
+	else
+	{
+		cout << "Error\n";
+	}
+	
+	
+		cout << "7.Which animal lays the largest eggs? \n A) peacock \n B) pigeon \n C) ostrich \n D) raven \n ";
+    cin >> answer;
+    
+    if (answer == 'C'  or answer =='c')
+    {
+    	cout << "correct!! \n";
+    	total_score = total_score + 1;
+	}
+	
+	else if (answer == 'B' || answer == 'A' || answer == 'D')
+	{
+	 cout << "Incorrect\n";	
+	 total_score = total_score;
+	}
+	else
+	{
+		cout << "Error\n";
+	}
+	
+	
+		cout << "8.What is the closest planet to the Sun? \n A) mecury \n B) jupiter \n C) earth \n D) uranus \n ";
+    cin >> answer;
+    
+    if (answer == 'A'  or answer =='a')
+    {
+    	cout << "correct!! \n";
+    	total_score = total_score + 1;
+	}
+	
+	else if (answer == 'B' || answer == 'C' || answer == 'D')
+	{
+	 cout << "Incorrect\n";	
+	 total_score = total_score;
+	}
+	else
+	{
+		cout << "Error\n";
+	}
+		cout << "9.In which country can you find the Eiffel Tower \n A) paris \n B) spain \n C) Nigeria \n D) Congo \n ";
+    cin >> answer;
+    
+    if (answer == 'A'  or answer =='a')
+    {
+    	cout << "correct!! \n";
+    	total_score = total_score + 1;
+	}
+	
+	else if (answer == 'B' || answer == 'C' || answer == 'D')
+	{
+	 cout << "Incorrect\n";	
+	 total_score = total_score;
+	}
+	else
+	{
+		cout << "Error\n";
+	}
+	
+	
+		cout << "10.How many Continents are there? \n A) 3 \n B) 2 \n C) 8 \n D) 7 \n ";
+    cin >> answer;
+    
+    if (answer == 'D'  or answer =='d')
+    {
+    	cout << "correct!! \n";
+    	total_score = total_score + 1;
+	}
+	
+	else if (answer == 'B' || answer == 'C' || answer == 'A')
+	{
+	 cout << "Incorrect\n";	
+	 total_score = total_score;
+	}
+	else
+	{
+		cout << "Error\n";
+	}
+	
+ 	return 0;
+ }
